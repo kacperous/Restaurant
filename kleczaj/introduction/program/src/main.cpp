913852c33786c4583612029b73c3dec2c18d46c4
Kacper Kleczaj <251551@edu.p.lodz.pl>
2024-03-11 21:46:25 +0100

    Math helper factorial:

--- kleczaj/introduction/program/src/main.cpp
+++ kleczaj/introduction/program/src/main.cpp
@@ -1,8 +1,8 @@
-//introduction project.
-
 #include <iostream>
 using namespace std;
+#include "math_helpers.h"
 int main(){
-	cout<<"hello world;
+	cout<<"hello world"<<endl;
+	cout<<"silnia(5): "<<factorial(5)<<endl;
 	return 0;
 }
