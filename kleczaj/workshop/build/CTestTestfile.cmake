ff48c47a8a3761a4d6b1c29a493c95f06ede966c
251551 Kleczaj Kacper <251551@edu.p.lodz.pl>
2024-03-12 11:01:56 +0100

    remove build folder

--- kleczaj/workshop/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/student/CLionProjects/start/kleczaj/introduction
-# Build directory: /home/student/CLionProjects/start/kleczaj/introduction/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("program")
-subdirs("library")
