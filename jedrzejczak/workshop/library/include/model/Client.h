8426056dfdc1a044ed9c79eb07e9eaf814f0a801
Mateusz Jędrzejczak <251536@edu.p.lodz.pl>
2024-03-17 23:29:13 +0100

    xd

--- jedrzejczak/workshop/library/include/model/Client.h
+++ jedrzejczak/workshop/library/include/model/Client.h
@@ -1,8 +1,30 @@
 #pragma once
+#include <string>
 
 class Client {
 public:
-    Client(); // Konstruktor bezparametrowy
-    ~Client(); // Destruktor
+    // Konstruktor parametrowy (zakładamy usunięcie konstruktora bezparametrowego)
+    Client(std::string firstName, std::string lastName, std::string personalID);
+
+    // Destruktor
+    ~Client();
+
+    // Metoda getInfo zwracająca informacje o kliencie
+    std::string getInfo() const;
+
+    // Gettery
+    std::string getFirstName() const;
+    std::string getLastName() const;
+    std::string getPersonalID() const;
+
+    // Settery
+    void setFirstName(const std::string& firstName);
+    void setLastName(const std::string& lastName);
+    void setPersonalID(const std::string& personalID);
+
+private:
+    std::string firstName;
+    std::string lastName;
+    std::string personalID;
 };
 
--- jedrzejczak/workshop/library/src/model/Client.cpp
+++ jedrzejczak/workshop/library/src/model/Client.cpp
@@ -1,11 +1,41 @@
-#include <iostream>
 #include "Client.h"
+#include <iostream>
+#include <sstream>
 
-Client::Client() {
-    std::cout << "Konstruktor bezparametrowy został wywołany." << std::endl;
+Client::Client(std::string firstName, std::string lastName, std::string personalID)
+    : firstName(firstName), lastName(lastName), personalID(personalID) {
+    std::cout << "Konstruktor parametrowy został wywołany: " << getInfo() << std::endl;
 }
 
 Client::~Client() {
-    std::cout << "Destruktor został wywołany." << std::endl;
+    std::cout << "Destruktor został wywołany: " << getInfo() << std::endl;
+}
+
+std::string Client::getInfo() const {
+    std::stringstream info;
+    info << "Klasa: Client, Imię: " << firstName << ", Nazwisko: " << lastName << ", PESEL: " << personalID;
+    return info.str();
+}
+
+std::string Client::getFirstName() const { return firstName; }
+std::string Client::getLastName() const { return lastName; }
+std::string Client::getPersonalID() const { return personalID; }
+
+void Client::setFirstName(const std::string& fName) {
+    if (!fName.empty()) {
+        firstName = fName;
+    }
+}
+
+void Client::setLastName(const std::string& lName) {
+    if (!lName.empty()) {
+        lastName = lName;
+    }
+}
+
+void Client::setPersonalID(const std::string& pID) {
+    if (!pID.empty()) {
+        personalID = pID;
+    }
 }
 
