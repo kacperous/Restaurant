90b60ea1e764027e1ddd346f6edf1c469e941ebd
251551 Kleczaj Kacper <251551@edu.p.lodz.pl>
2024-03-24 19:26:27 +0100

    usuwanie bo nie fajnie sie rzucilo

--- kleczaj/workshop/cmake-build-debug/program/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/student/CLionProjects/start/kleczaj/workshop/program
-# Build directory: /home/student/CLionProjects/start/kleczaj/workshop/cmake-build-debug/program
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
