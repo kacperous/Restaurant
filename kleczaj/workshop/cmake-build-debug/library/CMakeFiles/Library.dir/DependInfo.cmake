bc17a52ab924a30a11befddbe258d403309398a2
251551 Kleczaj Kacper <251551@edu.p.lodz.pl>
2024-03-24 19:20:08 +0100

    warsztaty2

---
+++ kleczaj/workshop/cmake-build-debug/library/CMakeFiles/Library.dir/DependInfo.cmake
@@ -0,0 +1,19 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/student/CLionProjects/start/kleczaj/workshop/library/src/model/Client.cpp" "library/CMakeFiles/Library.dir/src/model/Client.cpp.o" "gcc" "library/CMakeFiles/Library.dir/src/model/Client.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ kleczaj/workshop/cmake-build-debug/library/CMakeFiles/LibraryTester.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/LibraryTester.dir/test/ClientTest.cpp.o"
+  "CMakeFiles/LibraryTester.dir/test/ClientTest.cpp.o.d"
+  "CMakeFiles/LibraryTester.dir/test/master.cpp.o"
+  "CMakeFiles/LibraryTester.dir/test/master.cpp.o.d"
+  "LibraryTester"
+  "LibraryTester.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/LibraryTester.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
