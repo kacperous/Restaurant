009602cadf1422b5506aed63f7a51009dc727630
Mateusz Jędrzejczak <251536@edu.p.lodz.pl>
2024-03-24 17:57:17 +0100

    test

--- kleczaj/introduction/program/src/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-#include "math_helpers.h"
-int main(){
-	cout<<"hello world"<<endl;
-	cout<<"silnia(5): "<<factorial(5)<<endl;
-	cout<<"test czy wrzuca to na gita"<<endl;
-	return 0;
-}
