298693a8c879d4e45d2630ad902eff6001abea94
251551 Kleczaj Kacper <251551@edu.p.lodz.pl>
2024-06-04 13:08:30 +0200

    dodanie RestaurantPtr

--- Restaurant/start/library/include/model/typedefs.h
+++ Restaurant/start/library/include/model/typedefs.h
@@ -6,12 +6,14 @@ class ClientType;
 class Address;
 class Table;
 class Reservation;
+class Restaurant;
 
 typedef std::shared_ptr<Client> ClientPtr;
 typedef std::shared_ptr<ClientType> ClientTypePtr;
 typedef std::shared_ptr<Address> AddressPtr;
 typedef std::shared_ptr<Table> TablePtr;
 typedef std::shared_ptr<Reservation> ReservationPtr;
+typedef std::shared_ptr<Restaurant> RestaurantPtr;
 
 
 #endif //RESTAURANT_TYPEDEFS_H
